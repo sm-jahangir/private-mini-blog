cabb84bf1a1050f1043c05406774570a5f0061f2
sm-jahangir <samraatjahangir@gmail.com>
2021-09-05 20:09:00 +0600

    seeding complete

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -14,8 +14,9 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        Post::create([
+        $post1 = Post::create([
             'title' => 'Altec Lansing True Evo+ Truly Wireless Earphones',
+            'image' => '1.jpg',
             'user_id' => 3,
             'excerpt' => 'Altec Lansing True Evo+ Truly Wireless Earphones, 4 Hours of Battery Life, Receive Up to 4 Charges on The Go, Access Siri or Google Voice Assistant via Bluetooth Through Your Smartphone, MZX659-BLK ',
             'body' => "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cur post Tarentum ad Archytam? An est aliquid, quod te sua sponte delectet? Quos quidem tibi studiose et diligenter tractandos magnopere censeo. </p>
@@ -39,85 +40,127 @@ class PostSeeder extends Seeder
             'trending' => true,
             'popular' => true,
         ]);
-        Post::create([
+        $post1->categories()->attach(1);
+        $post1->tags()->attach(1);
+
+        $post11 = Post::create([
             'title' => 'Charming Evening Field',
+            'image' => '2.jpg',
             'user_id' => 3,
             'excerpt' => 'No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely',
             'body' => "No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremelyNo one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremelyNo one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremelyNo one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely",
             'trending' => true,
             'popular' => true,
         ]);
-        Post::create([
+        $post11->categories()->attach(1);
+        $post11->tags()->attach(1);
+
+        $post2 = Post::create([
             'title' => 'Traffic Jams Solved ',
+            'image' => '3.jpg',
             'user_id' => 3,
             'excerpt' => 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings',
             'body' => "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachingsBut I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachingsBut I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachingsBut I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings",
             'trending' => true,
             'popular' => true,
         ]);
-        Post::create([
+        $post2->categories()->attach(1);
+        $post2->tags()->attach(1);
+
+        $post3 = Post::create([
             'title' => 'Cliff Sunset Sea View',
+            'image' => '4.jpg',
             'user_id' => 3,
             'excerpt' => 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings...',
             'body' => "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings...But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings...But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings...But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings...",
             'trending' => true,
             'popular' => true,
         ]);
-        Post::create([
+        $post3->categories()->attach(1);
+        $post3->tags()->attach(1);
+
+        $post4 = Post::create([
             'title' => 'Trendy Summer Fashion',
+            'image' => '5.jpg',
             'user_id' => 3,
             'excerpt' => 'To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...',
             'body' => "To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...",
             'trending' => true,
             'popular' => true,
         ]);
-        Post::create([
+        $post4->categories()->attach(2);
+        $post4->tags()->attach(2);
+
+        $post5 = Post::create([
             'title' => 'New Fashion Outfits',
+            'image' => '6.jpg',
             'user_id' => 3,
             'excerpt' => 'To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...',
             'body' => "To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...",
             'trending' => true,
             'popular' => true,
         ]);
-        Post::create([
+        $post5->categories()->attach(2);
+        $post5->tags()->attach(2);
+
+        $post6 = Post::create([
             'title' => 'No one rejects, dislikes, or avoids pleasure',
+            'image' => '7.jpg',
             'user_id' => 3,
             'excerpt' => 'To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...',
             'body' => "To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...",
             'trending' => true,
             'popular' => true,
         ]);
-        Post::create([
+        $post6->categories()->attach(3);
+        $post6->tags()->attach(3);
+
+        $post7 = Post::create([
             'title' => 'To take a trivial example, which of us ever undertakes',
+            'image' => '8.jpg',
             'user_id' => 3,
             'excerpt' => 'To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...',
             'body' => "To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...",
             'trending' => true,
             'popular' => true,
         ]);
-        Post::create([
+        $post7->categories()->attach(3);
+        $post7->tags()->attach(3);
+
+        $post8 = Post::create([
             'title' => 'Trendy Summer Fashion',
+            'image' => '9.jpg',
             'user_id' => 3,
             'excerpt' => 'To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...',
             'body' => "To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...",
             'trending' => true,
             'popular' => true,
         ]);
-        Post::create([
+        $post8->categories()->attach(4);
+        $post8->tags()->attach(4);
+
+        $post9 = Post::create([
             'title' => 'These cases are perfectly simple and easy to distinguish',
+            'image' => '10.jpg',
             'user_id' => 3,
             'excerpt' => 'To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...',
             'body' => "To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...",
             'trending' => true,
             'popular' => true,
         ]);
-        Post::create([
+        $post9->categories()->attach(5);
+        $post9->tags()->attach(5);
+
+        $post10 = Post::create([
             'title' => 'welcomed and every pain avoided',
+            'image' => '11.jpg',
             'user_id' => 3,
             'excerpt' => 'To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...',
             'body' => "To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses...",
             'trending' => true,
             'popular' => true,
         ]);
+        $post10->categories()->attach(5);
+        $post10->tags()->attach(5);
     }
 }
